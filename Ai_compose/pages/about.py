33c7a644f28e445f5ae9944c1b1f4157fcf2a945
xsismadn3ss <2477carlos.a@gmail.com>
2024-08-28 03:05:16 -0600

    rederizar readme en about

--- Ai_compose/pages/about.py
+++ Ai_compose/pages/about.py
@@ -2,4 +2,9 @@ import reflex as rx
 
 @rx.page(route="/about", title="About")
 def about():
-    return rx.text("About")
+    with open("README.md", encoding="utf-8") as readme:
+        content = readme.read()
+    return rx.vstack(
+        rx.color_mode.button(position="top-right"),
+        rx.markdown(content)
+    )
