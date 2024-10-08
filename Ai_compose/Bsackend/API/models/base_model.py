3c03b363e295ca8e799f4058d4b51de88626ff01
xs_ismand3ss <2477carlos.a@gmail.com>
2024-10-08 07:06:21 -0600

    create basemodel request

---
+++ Ai_compose/Bsackend/API/models/base_model.py
@@ -0,0 +1,8 @@
+from ..api_client import API
+
+
+class BaseModel:
+    def __init__(self, api: API, **routes: str):
+        self.__api = api
+        for key, value in routes.items():
+            setattr(self, key, value)
