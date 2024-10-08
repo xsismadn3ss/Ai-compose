26f7330ff1ede60af142c0cf8191cdb3fe5ea207
xs_ismand3ss <2477carlos.a@gmail.com>
2024-10-08 07:08:41 -0600

    add api config

---
+++ Ai_compose/Bsackend/API/api_config.py
@@ -0,0 +1,37 @@
+from .models import *
+from .api_client import Routes, API
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+routes = Routes(
+    tones="/tones/",
+    scales="/scales/",
+    chords="/chords/",
+    notes="/notes/",
+    login="/login/",
+    register="/register/",
+    account="/account/{}",
+    chat="/chat/{}",
+    new_chat="/chat/create/",
+)
+
+print(getenv("SERVER_URL"))
+
+api = API(
+    server_url=getenv("SERVER_URL"),
+    headers={"Accepts": "*/*", "Authorization": "", "Content-Type": "application/json"},
+)
+
+# completado ✅
+user = User(
+    api=api, login=routes.login, register=routes.register, account=routes.register
+)
+chat = Chat(api=api, chat=routes.chat, new_chat=routes.new_chat)
+
+# esperando implementación de backtracking en el backend...
+tones = Tones(api=api, tones=routes.tones)
+notes = Notes(api=api, notes=routes.notes)
+scales = Scales(api=api, scales=routes.scales)
+chords = Chords(api=api, chords=routes.chords)
