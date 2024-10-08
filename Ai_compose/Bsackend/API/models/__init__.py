722d650a9b177e156f530e5a70cd1422208709bc
xs_ismand3ss <2477carlos.a@gmail.com>
2024-10-08 07:08:29 -0600

    import request models

---
+++ Ai_compose/Bsackend/API/models/__init__.py
@@ -0,0 +1,13 @@
+"""
+### modelos importadas
+* Notes,
+* Tones,
+* Chords
+* Scales
+* User
+* Chat
+"""
+
+from .music import Notes, Tones, Chords, Scales
+from .user import User
+from .chat import Chat
