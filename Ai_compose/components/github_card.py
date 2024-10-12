8739f4918263a0134f79c886f2c03fb5ddbcee0f
xsismadn3ss <2477carlos.a@gmail.com>
2024-10-12 13:11:10 -0600

    controlar excepciones en tarjete de github

--- Ai_compose/components/github_card.py
+++ Ai_compose/components/github_card.py
@@ -1,36 +1,62 @@
 import reflex as rx
 import requests
 
-def github_card(username: str, description:str="")-> rx.Component:
-    github_data = requests.get(f"https://api.github.com/users/{username}").json()
-    url = github_data["html_url"]
-    profile_image = github_data["avatar_url"]
-    name = github_data['name']
-    
-    if len(name) > 10:
-        name = name[0:10] + '...'
-        
+
+def github_card(username: str, description: str = "") -> rx.Component:
+    try:
+        github_data = requests.get(f"https://api.github.com/users/{username}").json()
+        profile_image = github_data["avatar_url"]
+        name = github_data["name"]
+        url = github_data["html_url"]
+        if len(name) > 10:
+            name = name[0:10] + "..."
+    except Exception as e:
+        url = f"https:/github.com/{username}"
+        name = None
+
     return rx.card(
         rx.link(
             rx.vstack(
-                rx.avatar(src=profile_image, radius="full", size="7", margin_bottom='0.5rem'),
+                rx.avatar(
+                    src=profile_image,
+                    fallback=f"{username[0:2]}".upper(),
+                    radius="full",
+                    size="7",
+                    margin_bottom="0.5rem",
+                ),
                 rx.color_mode_cond(
                     rx.text(name, size="3", weight="bold", color="#202020"),
                     rx.text(name, size="3", weight="bold", color="#d1d1d1"),
                 ),
                 rx.color_mode_cond(
-                    rx.text('@'+username, size="1", weight="regular", color="#202020"),
-                    rx.text('@'+username, size="1", weight='regular', color="#d1d1d1"),
+                    rx.text(
+                        "@" + username, size="1", weight="regular", color="#202020"
+                    ),
+                    rx.text(
+                        "@" + username, size="1", weight="regular", color="#d1d1d1"
+                    ),
                 ),
                 rx.color_mode_cond(
-                    rx.text(description, size="2", weight='medium', color="#202020", margin_top="0.6rem"),
-                    rx.text(description, size="2", weight='medium', color="#d1d1d1", margin_top='0.6rem'),
+                    rx.text(
+                        description,
+                        size="2",
+                        weight="medium",
+                        color="#202020",
+                        margin_top="0.6rem",
+                    ),
+                    rx.text(
+                        description,
+                        size="2",
+                        weight="medium",
+                        color="#d1d1d1",
+                        margin_top="0.6rem",
+                    ),
                 ),
                 align="center",
-                spacing='0'
+                spacing="0",
             ),
             href=url,
         ),
         width="12rem",
-        size="3"
+        size="3",
     )
