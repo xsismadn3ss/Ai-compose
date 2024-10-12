6f6683a021ca4e9f5484c39b12b2b0225968988f
xsismadn3ss <2477carlos.a@gmail.com>
2024-10-12 04:02:40 -0600

    create master template

---
+++ Ai_compose/templates/master.py
@@ -0,0 +1,33 @@
+from typing import Callable
+import reflex as rx
+
+from ..components.footer import footer
+from ..components.navbar.navbar import navbar
+
+
+def template(page: Callable[[], rx.Component]) -> rx.Component:
+    return rx.flex(
+        rx.flex(
+            navbar(),
+            padding="0",
+            width="100%",
+            direction="column",
+            style={
+                "position": "fixed",
+                "top": "0",
+                "background": 'transparent',
+                'backdrop_filter': 'blur(20px)'
+            },
+            z_index='1000'
+        ),
+        rx.flex(
+            page(),
+            style={"margin_top": "5rem", 'padding': '0.5rem'},
+            direction="column",
+            width='100%'
+        ),
+        rx.flex(margin_bottom="8rem"),
+        footer(),
+        width="100%",
+        direction='column'
+    )
