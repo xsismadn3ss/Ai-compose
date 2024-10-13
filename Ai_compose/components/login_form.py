f0261c65edb82ee1b00db5ec44185a981e07aa9e
xsismadn3ss <2477carlos.a@gmail.com>
2024-10-13 02:02:45 -0600

    update form dialog

--- Ai_compose/components/login_form.py
+++ Ai_compose/components/login_form.py
@@ -1,37 +1,88 @@
 import reflex as rx
+from Ai_compose.Session.session import Session
+
+
+class LoginForm_State(rx.State):
+    form_data: dict = {}
+
+    def handle_submit(self, form_data:dict):
+        self.form_data = form_data
+        is_authenticated = Session.procces_login(self.form_data)
+        
+        if is_authenticated:
+            return rx.toast.success('Inicio de sesión exitoso')
+
+        return rx.toast.error('Usuario o contraseña incorrrectos')
 
 def login_form():
+    return rx.form.root(
+        rx.form.field(
+            rx.flex(
+                rx.form.label("Nombre de usuario"),
+                rx.form.control(
+                    rx.input(
+                        placeholder="ingresa tu nombre de usuario",
+                        type="text",
+                    ),
+                    as_child=True,
+                ),
+                direction="column",
+                spacing="1",
+                align="stretch",
+            ),
+            name="username",
+        ),
+        rx.form.field(
+            rx.flex(
+                rx.form.label("Contraseña"),
+                rx.form.control(
+                    rx.input(placeholder="ingresa tu contraseña", type="password"),
+                    as_child=True,
+                ),
+                direction="column",
+                spacing="1",
+                align="stretch",
+            ),
+            name='password'
+        ),
+        rx.flex(
+            rx.link(
+                "¿No tienes una cuenta? Registrate aquí",
+                href="/sign_up",
+                size="2",
+                color_scheme="purple",
+            ),
+        ),
+        rx.flex(
+            rx.form.submit(
+                rx.button("Ingresar"),
+                as_child=True,
+            ),
+            spacing='2',
+            justify='center',
+            margin_top="1rem"
+        ),
+        on_submit=lambda form_data: LoginForm_State.handle_submit(form_data),
+        reset_on_submit=True,
+    )
+
+
+def login_form_dialog():
     return rx.dialog.content(
         rx.flex(
             rx.card(
                 rx.flex(
-                    rx.icon("music-2"), rx.heading(" Ai Compose"),
-                    direction="row",                      
+                    rx.icon("music-2"),
+                    rx.heading(" Ai Compose"),
+                    direction="row",
                 ),
             ),
             rx.heading("Ingresa tus credenciales", as_="h4"),
             direction="column",
             align="center",
-            spacing="2"          
+            spacing="2",
         ),
-        rx.flex(
-            rx.text("Username", weight="bold", margin_top="1vh"), rx.input(placeholder="Ingresa tu nombre de usuario"),
-            rx.text("Email", weight="bold"), rx.input(placeholder="correo@example.com"),
-            rx.text("Password", weight="bold"), rx.input(placeholder="Ingresa tu contraseña"),
-            rx.link("¿No tienes una cuenta? Registrate aquí", href="/sign_up", size="2", color_scheme='purple'),
-            spacing="4",
-            direction="column",
-            margin="auto 2vh 2vh"
-        ),
-        rx.dialog.close(
-            rx.flex(
-                rx.button("Cancelar"),
-                rx.button("Ingresar"),
-                justify="center",
-                spacing="3"
-            ),
-        ),
-        width="30vw",
+        login_form(),
         padding="2em",
         max_width="28em",
     )
