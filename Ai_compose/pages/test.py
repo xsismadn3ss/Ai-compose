f5ac22081410ec68c2226bdc312c0e3f5662b8e9
Abraham <132122513+xsismadn3ss@users.noreply.github.com>
2024-10-14 07:35:21 +0000

    hacer pruebas para manejar inicio de sesión

--- Ai_compose/pages/test.py
+++ Ai_compose/pages/test.py
@@ -1,50 +1,30 @@
 import reflex as rx
-
-from Ai_compose.components.cards import features_cards
-from Ai_compose.components.github_card import github_card
-from Ai_compose.components.login_form import login_form
+from rxconfig import glow_bg_light
 from ..templates.master import template
-from Ai_compose.Session.sesionState import SessionState
+from ..state.auth import AuthState
+
+def login_card(auth):
+    return rx.vstack(
+        rx.text("Nombre de usuario"),
+        rx.input(placeholder="ingresa tu nombre de usuario", name='username', on_blur=auth.set_username),
+        rx.text("Contraseña"),
+        rx.input(placeholder="ingresa tu contraseña", name='password', type='password', on_blur=auth.set_password),
+        rx.button("Enviar", on_click=auth.login),
+    )
 
 
 @rx.page(route="/test", title="Test")
 @template
-def test_page():
+def test_page(auth: AuthState):
     return rx.center(
         rx.container(
-            rx.heading(
-                "Aprende de música. Sin límites.",
-                as_="h2",
-                align="center",
-                trim="normal",
-                size="8",
-                margin_top="20vh",
-            ),
-            rx.text(
-                "Herramientas profesionales para aprender sobre teoría musical al alcance de todos.",
-                align="center",
-                size="5",
-                margin_bottom="2vh",
-            ),
-        ),
-        rx.card(
-            login_form(),
-            width="20rem"
-        ),
-        rx.divider(),
-        rx.text(
-            "Créditos", weight="bold", size="6", margin_bottom="5vh", margin_top="5vh"
-        ),
-        rx.flex(
-            github_card(username="Haluuuu", description='Backend developer'),
-            github_card(username="xsismadn3ss", description='Fullstack developer'),
-            github_card("Alexandra-Rivera", description='Frontend developer'),
-            direction="row",
-            justify="center",
-            align="center",
-            spacing="5",
-            margin_bottom="5vh",
-            wrap='wrap'
+            login_card(auth),
+            rx.divider(),
+            rx.cond(
+                auth.logged_in,
+                rx.text("Sesión iniciada"),
+                rx.text("Sesión cerrada"),
+            )
         ),
         ##########
         direction="column",
