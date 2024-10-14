f2cf64cffb4ecbc15e4d1bf8975650eb590663e9
Abraham <132122513+xsismadn3ss@users.noreply.github.com>
2024-10-14 07:25:00 +0000

    agregar como parametro por defecto el estado de autentificacion en la pagina

--- Ai_compose/templates/master.py
+++ Ai_compose/templates/master.py
@@ -3,12 +3,18 @@ import reflex as rx
 
 from ..components.footer import footer
 from ..components.navbar.navbar import navbar
+from ..state.auth import AuthState
 
 
-def template(page: Callable[[], rx.Component]) -> rx.Component:
+def template(page: Callable[[], rx.Component], is_required:bool=False) -> rx.Component:
+    if is_required:
+        return  rx.redirect('/')
+
+
+
     return rx.flex(
         rx.flex(
-            navbar(),
+            navbar(AuthState),
             padding="0",
             width="100%",
             direction="column",
@@ -21,7 +27,7 @@ def template(page: Callable[[], rx.Component]) -> rx.Component:
             z_index='1000'
         ),
         rx.flex(
-            page(),
+            page(AuthState),
             style={"margin_top": "5rem", 'padding': '0.5rem'},
             direction="column",
             width='100%'
