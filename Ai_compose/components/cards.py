5b67cbc7f91751759411d6b1981d51e4cafa7156
Abraham <132122513+xsismadn3ss@users.noreply.github.com>
2024-10-14 08:20:15 +0000

    actualizar diseño de tarjetas y agregar tarjeta nueva

--- Ai_compose/components/cards.py
+++ Ai_compose/components/cards.py
@@ -1,24 +1,20 @@
 import reflex as rx
-# from rxconfig import card_style
-
-glow_bg_light = {
-    'box_shadow': '0px 0px 4.5rem 1rem #9563ee',
-}
+from rxconfig import glow_bg_light
 
 
 def feature_card(text: str, **kwargs):
     return rx.card(
         rx.flex(
-            rx.text(text, weight='bold'),
+            rx.text(text, weight="bold"),
             direction="column",
             align="center",
-            justify='center',
+            justify="center",
             height="10rem",
             width="15rem",
         ),
         rx.flex(style=glow_bg_light),
         text_align="center",
-        size='3',
+        size="3",
         **kwargs
     )
 
@@ -30,8 +26,19 @@ def features_cards():
         feature_card("Chatbot con IA incluido"),
         feature_card("Documentación de teoría músical gratis"),
         spacing="4",
-        justify='center',
-        margin_top='3rem',
-        margin_bottom='5rem',
+        justify="center",
+        margin_top="3rem",
+        margin_bottom="5rem",
         wrap="wrap",
+    )
+
+
+def not_logged_ind_card():
+    return rx.card(
+        rx.flex(style=glow_bg_light),
+        rx.heading("Necesitas iniciar sesión", size="5", margin_bottom="0.8rem"),
+        rx.text("Inicia sesión para acceder a este contenido", margin_bottom="0.8rem"),
+        rx.button("Ir a inicio", on_click=rx.redirect("/")),
+        margin_top="4rem",
+        size="3",
     )
