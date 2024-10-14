33124d1a622718caab1f09d4deb893dc71810ddc
Abraham <132122513+xsismadn3ss@users.noreply.github.com>
2024-10-14 08:21:01 +0000

    añadir condición de renderizado para la pagina de piano roll

--- Ai_compose/pages/pianoroll.py
+++ Ai_compose/pages/pianoroll.py
@@ -1,26 +1,37 @@
 import reflex as rx
 
+from ..state.auth import AuthState
 from ..templates.master import template
-from ..components.navbar.navbar import navbar
-from ..components.footer import footer
 from ..components.piano_component import piano_roll
+from ..components.cards import not_logged_ind_card as message_card
+    
 
 
 @rx.page(route="/piano_roll", title="Piano roll test")
 @template
-def painoRoll():
-    return rx.center(
-        rx.container(
-            rx.heading(
-                "Generador de Acordes y Tonalidades", as_="h3", margin_top="10vh"
+def painoRoll(auth: AuthState):
+    return rx.cond(
+        auth.logged_in,
+        rx.center(
+            rx.container(
+                rx.heading(
+                    "Generador de Acordes y Tonalidades", as_="h3", margin_top="10vh"
+                ),
+                rx.center(piano_roll(), margin_bottom="10vh"),
+                rx.heading("Generador de Escalas", as_="h3", margin_bottom="5vh"),
+                rx.center(
+                    piano_roll(),
+                    margin_bottom="10vh",
+                ),
             ),
-            rx.center(piano_roll(), margin_bottom="10vh"),
-            rx.heading("Generador de Escalas", as_="h3", margin_bottom="5vh"),
-            rx.center(
-                piano_roll(),
-                margin_bottom="10vh",
-            ),
+            direction="column",
+            justify="center",
         ),
-        direction="column",
-        justify="center",
+        rx.center(
+            rx.vstack(
+                message_card(),
+                align="center",
+                justify="center",
+            )
+        ),
     )
