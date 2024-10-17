108abc14d95bfc1d005c59861802e67ae4dbec17
xsismadn3ss <2477carlos.a@gmail.com>
2024-10-17 11:07:35 -0600

    renombrar componentes, actualizar  comportamiento y empezar implementacion para selector de tipo de acordes

--- Ai_compose/components/piano/chordmaker/screen.py
+++ Ai_compose/components/piano/chordmaker/screen.py
@@ -1,19 +1,25 @@
 import reflex as rx
 from .state import ChordMakerState
 
+light_border = "1px solid #d3d3d3"
+dark_border = "1px solid #414141"
+
 def result():
     return rx.flex(
         rx.text(
             ChordMakerState.get_data,
-            color=rx.color_mode_cond("white", "white"),
+            color=rx.color_mode_cond("#303030", "#bdbdbd"),
             margin_x="1rem",
         ),
         rx.text(
             ChordMakerState.symbol,
-            color=rx.color_mode_cond("white", "white"),
+            color=rx.color_mode_cond("#303030", "#bdbdbd"),
             margin_right="1rem",
         ),
-        bg="black",
+        bg=rx.color_mode_cond(
+            light="#bdbdbd",
+            dark='#303030'
+        ),
         padding_y="0.5rem",
         border_top_left_radius="0.8rem",
         border_bottom_left_radius="0.8rem",
@@ -21,23 +27,48 @@ def result():
 
 none_func = lambda: None
 
-def screen_button(icon_name:str, on_click = none_func(), tooltip: str = ""):
-    light_border = "1px solid #d3d3d3"
-    dark_border = "1px solid #414141"
-    return rx.flex(
+def icon_button(icon_name:str, on_click, tooltip: str = ""):
+    return rx.tooltip(
+        rx.flex(
+            rx.icon_button(
+                icon_name,
+                color_scheme="gray",
+                variant="ghost",
+                cursor="pointer",
+                margin_x="0.5rem",
+                on_click=on_click,
+            ),
+            border_left=rx.color_mode_cond(light=light_border, dark=dark_border),
+            padding_y="0.5rem",
+        ),
+        content=tooltip
+    )
+
+def drop_button(icon_name:str, tooltip:str=""):
+    return rx.tooltip(
         rx.icon_button(
             icon_name,
             color_scheme="gray",
-            variant="ghost",
-            cursor="pointer",
-            margin_x="0.5rem",
-            on_click=on_click
+            variant='ghost',
+            cursor='pointer',
+            margin_x='0.5rem',
+            padding_y='0.5rem'
         ),
-        border_left=rx.color_mode_cond(light=light_border, dark=dark_border),
-        padding_y="0.5rem",
+        content=tooltip
     )
 
+
 def chord_selector():
+    return rx.menu.root(
+        rx.menu.trigger(
+            drop_button('chevron-down', tooltip='Seleccionar tipo de acordes')
+        ),
+        rx.menu.content(
+            rx.menu.item(
+                rx.heading("Selecciona un tipo de acorde", size='2', weight='medium')
+            )
+        )
+    )
     # TODO: agregar drop down para seleccionar tipo de acorde
     # TODO: obtener la lista de proporciones utilizando la api
     ...
@@ -50,9 +81,9 @@ def screen():
     return rx.flex(
         rx.flex(
             result(),
-            screen_button("chevron-down"),
-            screen_button("delete", on_click=ChordMakerState.clear()),
-            screen_button("send-horizontal", on_click=ChordMakerState.generate_chord()),
+            chord_selector(),
+            icon_button(icon_name="delete", on_click=ChordMakerState.clear(), tooltip='Borrar todo'),
+            icon_button(icon_name="send-horizontal", on_click=ChordMakerState.generate_chord(), tooltip='Generar acorde'),
         ),
         border=rx.color_mode_cond(light=light_border, dark=dark_border),
         text_align="center",
