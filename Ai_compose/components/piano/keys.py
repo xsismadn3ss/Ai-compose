35c25f0ba8ff7f73a7c4321403d84c0aca55022f
RIVERA MENDEZ ALEXANDRA BEATRIZ <91226679+Alexandra-Rivera@users.noreply.github.com>
2024-10-21 07:00:32 +0000

    agregar condiciones para teclas

--- Ai_compose/components/piano/keys.py
+++ Ai_compose/components/piano/keys.py
@@ -1,10 +1,19 @@
 import reflex as rx
 from rxconfig import black_keys_style, white_keys_style
 
+
 def key(note: str, is_black: bool = False, on_click=None) -> rx.Component:
     if is_black:
         style = black_keys_style
+
     else:
         style = white_keys_style
 
-    return rx.button(note.upper(), style=style, on_click=on_click)
+    return (
+        rx.tablet_and_desktop(
+            rx.button(note.upper(), style=style, on_click=on_click, size="3"),
+        ),
+        rx.mobile_only(
+            rx.button(note.upper(), style=style, on_click=on_click, size="2")
+        ),
+    )
