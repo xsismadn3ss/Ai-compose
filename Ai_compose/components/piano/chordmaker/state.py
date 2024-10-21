bfc1fb675869a188b066a1a4440e924b756aed85
RIVERA MENDEZ ALEXANDRA BEATRIZ <91226679+Alexandra-Rivera@users.noreply.github.com>
2024-10-21 07:05:50 +0000

    actualizar clase de estado para generador de acordes

--- Ai_compose/components/piano/chordmaker/state.py
+++ Ai_compose/components/piano/chordmaker/state.py
@@ -1,10 +1,15 @@
 import reflex as rx
+import time
 from ....API.api_config import chords
 
 
 class ChordMakerState(rx.State):
     note: str = "_"
     symbol: str = "_"
+    is_error: bool = False
+    is_loaded: bool = False
+    notes: list[str] = []
+    data: dict
 
     def set_note(self, note: str):
         self.note = note.upper()
@@ -17,22 +22,21 @@ class ChordMakerState(rx.State):
 
     def clear(self):
         self.reset()
+        self.is_loaded = False
 
     def generate_chord(self):
         if self.note == "_" or self.symbol == "_":
-            return rx.toast.error(
-                "Asegurate de seleccionar una nota y una proporción antes de generar un acorde",
-                position="top-center",
-            )
+            self.is_error = True
+            self.is_loaded = False
+            return
 
         response = chords.generate(note=self.note, symbol=self.symbol)
-
-        return rx.toast.info(
-            " Generando acorde {} {}, Acorde: {}".format(
-                self.note, self.symbol, response
-            ),
-            position="top-center",
-        )
+        self.data = response
+        self.notes = response['notes']
+        time.sleep(0.2)
+        self.is_loaded = True
+        self.is_error = False
+        return
 
     @rx.var
     def get_data(self):
