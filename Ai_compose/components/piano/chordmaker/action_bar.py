22d228f4a20db0a06e5486fd07bd8920503e8e60
RIVERA MENDEZ ALEXANDRA BEATRIZ <91226679+Alexandra-Rivera@users.noreply.github.com>
2024-10-22 00:29:54 +0000

    actualizar barra de acciones

--- Ai_compose/components/piano/chordmaker/action_bar.py
+++ Ai_compose/components/piano/chordmaker/action_bar.py
@@ -1,25 +1,15 @@
-import reflex as rx
-from rxconfig import light_border, dark_border
 from .state import ChordMakerState
 from .dropdown import dropdown
-from .icon_button import icon_button
-from .prop_screen import prop_screen
+from ..icon_button import icon_button
 from .result_dialog import result_dialog
+from ..actionbar import action_bar
+from .prop_screen import prop_screen
 
-def action_bar():
 
-    return rx.flex(
-        rx.flex(
-            prop_screen(),
-            dropdown(),
-            icon_button(icon_name="delete", on_click=ChordMakerState.clear()),
-            result_dialog(),
-        ),
-        border=rx.color_mode_cond(light=light_border, dark=dark_border),
-        text_align="center",
-        justify="center",
-        border_radius="0.8rem",
-        width="fit-content",
-        margin_y="1rem",
-        align="center",
+def bar():
+    return action_bar(
+        prop_screen(),
+        dropdown(),
+        icon_button(icon_name="delete", on_click=ChordMakerState.clear()),
+        result_dialog(),
     )
