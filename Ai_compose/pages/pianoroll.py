04a4aeb2271d4620dc0f6705f89a790dab1699ee
Abraham <132122513+xsismadn3ss@users.noreply.github.com>
2024-10-22 14:05:11 +0000

    actualizar pagina de pianoroll

--- Ai_compose/pages/pianoroll.py
+++ Ai_compose/pages/pianoroll.py
@@ -5,7 +5,7 @@ from ..templates.master import template
 from ..components.piano_component import piano_roll
 from ..components.cards import not_logged_ind_card as message_card
 from ..components.piano.chordmaker.chord_maker import chord_maker
-    
+from ..components.piano.tonemaker.tone_maker import tone_maker
 
 
 @rx.page(route="/piano_roll", title="Piano roll test")
@@ -15,10 +15,25 @@ def painoRoll(auth: AuthState):
         auth.logged_in,
         rx.center(
             rx.container(
-                rx.heading(
-                    "Generador de Acordes y Tonalidades", as_="h3", margin_top="10vh"
+                rx.heading("Acordes y Tonalidades", as_="h3", margin_top="10vh"),
+                rx.center(
+                    rx.tabs.root(
+                        rx.tabs.list(
+                            rx.tabs.trigger("Generar acordes", value="tab1"),
+                            rx.tabs.trigger("Generar escalas", value="tab2"),
+                        ),
+                        rx.tabs.content(
+                            chord_maker(),
+                            value="tab1",
+                        ),
+                        rx.tabs.content(
+                            tone_maker(),
+                            value="tab2",
+                        ),
+                        default_value="tab1",
+                    ),
+                    margin_bottom="10vh",
                 ),
-                rx.center(chord_maker(), margin_bottom="10vh"),
                 rx.heading("Generador de Escalas", as_="h3", margin_bottom="5vh"),
                 rx.center(
                     piano_roll(),
