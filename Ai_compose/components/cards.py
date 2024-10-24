2105456a201e20f610a9faaa392c1f2f7c8173a6
RIVERA MENDEZ ALEXANDRA BEATRIZ <91226679+Alexandra-Rivera@users.noreply.github.com>
2024-10-24 15:36:47 +0000

    añadir iconos a las tarjetas

--- Ai_compose/components/cards.py
+++ Ai_compose/components/cards.py
@@ -2,16 +2,17 @@ import reflex as rx
 from rxconfig import glow_bg_light
 
 
-def feature_card(text: str, **kwargs):
+def feature_card(text: str, *args, **kwargs):
     return rx.card(
         rx.flex(
             rx.text(text, weight="bold"),
             direction="column",
             align="center",
             justify="center",
-            height="10rem",
+            height="9rem",
             width="15rem",
         ),
+        *args,
         rx.flex(style=glow_bg_light),
         text_align="center",
         size="3",
@@ -21,10 +22,38 @@ def feature_card(text: str, **kwargs):
 
 def features_cards():
     return rx.flex(
-        feature_card("Experimenta con Acordes"),
-        feature_card("Descubre Escalas y Tonalidades"),
-        feature_card("Chatbot con IA incluido"),
-        feature_card("Documentación de teoría músical gratis"),
+        feature_card(
+            "Experimenta con Acordes",
+            rx.flex(
+                rx.icon("music", color="gray"),
+                rx.icon("music-4", color="gray"),
+                spacing="2",
+            ),
+        ),
+        feature_card(
+            "Descubre Escalas y Tonalidades",
+            rx.flex(
+                rx.icon("file-music", color="gray"),
+                rx.icon("audio-waveform", color="gray"),
+                spacing="2",
+            ),
+        ),
+        feature_card(
+            "Chatbot con IA incluido",
+            rx.flex(
+                rx.icon("bot", color="gray"),
+                rx.icon("messages-square", color="gray"),
+                spacing="2",
+            ),
+        ),
+        feature_card(
+            "Documentación de teoría músical gratis",
+            rx.flex(
+                rx.icon("circle-check", color="gray"),
+                rx.icon("book-open-text", color="gray"),
+                spacing="2",
+            ),
+        ),
         spacing="4",
         justify="center",
         margin_top="3rem",
@@ -39,6 +68,17 @@ def not_logged_ind_card():
         rx.heading("Necesitas iniciar sesión", size="5", margin_bottom="0.8rem"),
         rx.text("Inicia sesión para acceder a este contenido", margin_bottom="0.8rem"),
         rx.button("Ir a inicio", on_click=rx.redirect("/")),
+        margin_top="4rem",
+        size="3",
+    )
+
+
+def register_succesfully():
+    return rx.card(
+        rx.flex(style=glow_bg_light),
+        rx.heading("Bienvenido a Ai Compose", size="5", margin_botton="0.8rem"),
+        rx.text("Ya has iniciado sesión"),
+        rx.button("Comienza a aprender", on_click=rx.redirect("/piano_roll")),
         margin_top="4rem",
         size="3",
     )
