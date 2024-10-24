199b457639905e47529e20d461f75c922ccbb62c
RIVERA MENDEZ ALEXANDRA BEATRIZ <91226679+Alexandra-Rivera@users.noreply.github.com>
2024-10-24 15:37:54 +0000

    añadir condicion en pagina de registro

--- Ai_compose/pages/sign_up.py
+++ Ai_compose/pages/sign_up.py
@@ -2,12 +2,18 @@ import reflex as rx
 
 from ..templates.master import template
 from ..components.forms.signupform import signup_form
+from ..components.cards import register_succesfully
+from ..state.auth_state import AuthState
 
 @rx.page(route="/sign_up", title="Registrarse")
 @template
 def signup()-> rx.Component:
     return rx.center(
-        signup_form(),
+        rx.cond(
+            AuthState.is_logged_in,
+            register_succesfully(),
+            signup_form()
+        ),
         direction="column",
         justify="center"
     )
