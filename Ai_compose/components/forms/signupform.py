67d1fb69160ca4621a948ecda221fb56623d0a84
RIVERA MENDEZ ALEXANDRA BEATRIZ <91226679+Alexandra-Rivera@users.noreply.github.com>
2024-10-24 15:37:10 +0000

    actualizar formulario de registro

--- Ai_compose/components/forms/signupform.py
+++ Ai_compose/components/forms/signupform.py
@@ -64,7 +64,7 @@ def form_content():
                 ),
                 spacing="2",
                 max_width="26rem",
-                width='auto',
+                width="auto",
             ),
             label("Correo electrónico"),
             field(
@@ -89,20 +89,17 @@ def form_content():
             ),
             rx.flex(
                 rx.button("Registrarse", type="submit"),
-                rx.link(
-                    rx.button('Cancelar'),
-                    href='/'
-                ),
+                rx.link(rx.button("Cancelar"), href="/"),
                 spacing="2",
                 justify="center",
-                margin_top='1rem'
+                margin_top="1rem",
             ),
             direction="column",
             margin_bottom="1.5rem",
             spacing="2",
         ),
         on_submit=AuthState.register,
-        reset_on_submit=True,
+        reset_on_submit=False,
         direction="column",
         maring_y="1rem",
         spacing="2",
@@ -110,98 +107,11 @@ def form_content():
 
 
 def signup_form():
-    return (
-        rx.card(
-            form_header(),
-            form_content(),
-            padding="3",
-            max_width="28em",
-            size="4",
-            width="100%",
-        ),
+    return rx.card(
+        form_header(),
+        form_content(),
+        padding="3",
+        max_width="28em",
+        size="4",
+        width="100%",
     )
-
-
-# def signup_form() -> rx.Component:
-#     return rx.card(
-#         rx.vstack(
-#             rx.center(
-#                 rx.image(
-#                     src="/logo.jpg",
-#                     width="2.5em",
-#                     height="auto",
-#                     border_radius="25%",
-#                 ),
-#                 rx.heading(
-#                     "Create an account",
-#                     size="6",
-#                     as_="h2",
-#                     text_align="center",
-#                     width="100%",
-#                 ),
-#                 direction="column",
-#                 spacing="5",
-#                 width="100%",
-#             ),
-#             rx.vstack(
-#                 rx.text(
-#                     "Email address",
-#                     size="3",
-#                     weight="medium",
-#                     text_align="left",
-#                     width="100%",
-#                 ),
-#                 rx.input(
-#                     rx.input.slot(rx.icon("user")),
-#                     placeholder="user@reflex.dev",
-#                     type="email",
-#                     size="3",
-#                     width="100%",
-#                 ),
-#                 justify="start",
-#                 spacing="2",
-#                 width="100%",
-#             ),
-#             rx.vstack(
-#                 rx.text(
-#                     "Password",
-#                     size="3",
-#                     weight="medium",
-#                     text_align="left",
-#                     width="100%",
-#                 ),
-#                 rx.input(
-#                     rx.input.slot(rx.icon("lock")),
-#                     placeholder="Enter your password",
-#                     type="password",
-#                     size="3",
-#                     width="100%",
-#                 ),
-#                 justify="start",
-#                 spacing="2",
-#                 width="100%",
-#             ),
-#             rx.box(
-#                 rx.checkbox(
-#                     "Agree to Terms and Conditions",
-#                     default_checked=True,
-#                     spacing="2",
-#                 ),
-#                 width="100%",
-#             ),
-#             rx.button("Register", size="3", width="100%"),
-#             rx.center(
-#                 rx.text("Already registered?", size="3"),
-#                 rx.link("Sign in", href="#", size="3"),
-#                 opacity="0.8",
-#                 spacing="2",
-#                 direction="row",
-#                 width="100%",
-#             ),
-#             spacing="6",
-#             width="100%",
-#         ),
-#         max_width="28em",
-#         size="4",
-#         width="100%",
-#     )
