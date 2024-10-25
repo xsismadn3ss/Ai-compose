297e9cf88b1cdd57318b04ef9809d0ba6fa0b6a8
xsismadn3ss <2477carlos.a@gmail.com>
2024-10-25 03:33:31 -0600

    actualizar componente markdown

--- Ai_compose/components/markdown_render.py
+++ Ai_compose/components/markdown_render.py
@@ -3,6 +3,6 @@ import reflex as rx
 
 def markdown(path: str = ""):
     with open(path, encoding="utf-8") as mk:
-        content = mk.read
+        content = mk.read()
 
     return rx.markdown(content)
