fe7874baffc181d0e89883f9c816e9e5e1b7af7d
xsismadn3ss <2477carlos.a@gmail.com>
2024-10-25 03:29:30 -0600

    crear pagina de introduccion para documentacion

--- Ai_compose/pages/docs/introduction.py
+++ Ai_compose/pages/docs/introduction.py
@@ -1,16 +1,26 @@
 import reflex as rx
 from ...templates.master import template
+from ...components.docs_menu.menu import doc_menu
 
 
-@rx.page(route='docs/', title='Documentación')
+@rx.page(route="/docs", title="Documentación")
 @template
 def introduction():
 
-    with open('docs\Música.md', encoding='utf-8') as readme:
-        content = readme
+    with open("./docs/Música.md", encoding="utf-8") as readme:
+        content = readme.read()
 
     return rx.container(
-        rx.card(rx.markdown(content), size='3'),
-        justify='center',
-        direction='column'
+        rx.card(
+            rx.flex(
+                doc_menu(blur='Introducción'),
+                justify="end",
+                position="sticky",
+                top="1rem",
+            ),
+            rx.markdown(content),
+            size='3',
+        ),
+        justify="center",
+        direction="column",
     )
