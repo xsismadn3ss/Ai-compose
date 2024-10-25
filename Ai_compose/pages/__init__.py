71193441af6b9d42f444b61fd917bbbd9160275e
xsismadn3ss <2477carlos.a@gmail.com>
2024-10-25 03:30:26 -0600

    añadir paginas de documentación

--- Ai_compose/pages/__init__.py
+++ Ai_compose/pages/__init__.py
@@ -4,6 +4,8 @@ from .login import login_page
 from .pianoroll import painoRoll
 from .sign_up import signup
 from .chat import chat
+from .docs.introduction import introduction
+from .docs.acordes import *
 
 # solo usar para test
 # from .test import test_page
