81811c12f69e91a62f95652523b4a5b0152ef9e6
xsismadn3ss <2477carlos.a@gmail.com>
2024-10-27 17:47:04 -0600

    añadir condicion de inicio de sesión

--- Ai_compose/pages/login.py
+++ Ai_compose/pages/login.py
@@ -1,10 +1,16 @@
 import reflex as rx
 from ..templates.master import template
 from ..components.forms.loginform import login_form
+from ..components.cards import register_succesfully as message_card
+from ..state.auth_state import AuthState
 
 @rx.page('/login', title='Iniciar sesión')
 @template
 def login_page():
     return rx.center(
-        login_form()
+        rx.cond(
+            AuthState.is_logged_in,
+            message_card(),
+            login_form(),
+        )
     )
