2d1079328d43d2ecf1b053eafb85f42be55458d6
xsismadn3ss <2477carlos.a@gmail.com>
2024-10-27 21:09:03 -0600

    crear plantilla para crear tajetas de documentacion

--- Ai_compose/pages/docs/views/container.py
+++ Ai_compose/pages/docs/views/container.py
@@ -1,8 +1,32 @@
 import reflex as rx
+from ....components.markdown_render import markdown
+from ....components.docs_menu.menu import doc_menu
 
-def container(*args):
+def doc_card(blur_menu_item:str, markdown_route:str):
+    """
+    ## Doc card
+    Crea un tarjeta para renderizar archivos markdown y mostrar en la seccion de documentación
+
+    Args
+    * blur_menu_item: especifica que titulo deseas sombrear en el menu, asegurate que ese item exista en el indice y se llame exactamente igual.
+    * markdown: especifica la ruta relativa del archivo markdown a renderizar.
+
+    ### Nota
+    Puedes agregar nuevos items en el indice ``components/docs_menu/menu.py``
+
+    """
     return rx.container(
-        args,
+        rx.card(
+            rx.flex(
+                doc_menu(
+                    blur=blur_menu_item
+                ),
+                markdown(
+                    markdown_route
+                ),
+                size="2"
+            )
+        ),
         justify='center',
         direction='column'
     )
