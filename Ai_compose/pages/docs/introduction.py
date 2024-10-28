35849e7eddd986b5018b186a90a9783ca93ec1f6
xsismadn3ss <2477carlos.a@gmail.com>
2024-10-27 20:46:51 -0600

    importar componente markdown

--- Ai_compose/pages/docs/introduction.py
+++ Ai_compose/pages/docs/introduction.py
@@ -1,25 +1,20 @@
 import reflex as rx
 from ...templates.master import template
 from ...components.docs_menu.menu import doc_menu
+from ...components.markdown_render import markdown
 
 
 @rx.page(route="/docs", title="Documentación")
 @template
 def introduction():
-
-    with open("./docs/Música.md", encoding="utf-8") as readme:
-        content = readme.read()
-
     return rx.container(
         rx.card(
             rx.flex(
-                doc_menu(blur='Introducción'),
+                doc_menu(blur="Introducción"),
                 justify="end",
-                position="sticky",
-                top="1rem",
             ),
-            rx.markdown(content),
-            size='3',
+            markdown("./docs/Música.md"),
+            size="3",
         ),
         justify="center",
         direction="column",
