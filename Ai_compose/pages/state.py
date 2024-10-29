9430776b4e18394574e34cf70c0f9c1b30accb8f
RIVERA MENDEZ ALEXANDRA BEATRIZ <91226679+Alexandra-Rivera@users.noreply.github.com>
2024-10-29 06:10:24 +0000

    axrualizar estado de la pagina

--- Ai_compose/pages/state.py
+++ Ai_compose/pages/state.py
@@ -5,43 +5,44 @@ from g4f.client import Client
 
 client = Client()
 
+
 class State(rx.State):
-    # The current question being asked.
-    question: str = ""  # Inicializa la pregunta como una cadena vacía
-    # Keep track of the chat history as a list of (question, answer) tuples.
-    chat_history: list[tuple[str, str]] = []  # Inicializa la lista de historial
+    question: str = ""
+    chat_history: list[tuple[str, str]] = []
+
+    def clear_history(self):
+        self.chat_history = []
+        return rx.toast.success("Iniciando un a nueva conversación")
 
     def answer(self):
-    # Our chatbot has some brains now!
-  
+
         session = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "user", "content": self.question}
-        ],stop=None,
-        temperature=0.7,
-        stream=True,
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "user",
+                    "content": self.question
+                    + "responde unicamente en español, y trata de responder todo unicamente relacionado a teoria musical",
+                }
+            ],
+            stop=None,
+            temperature=0.5,
+            stream=True,
         )
 
-     # Add to the answer as the chatbot responds.
         answer = ""
         self.chat_history.append((self.question, answer))
 
-         # Clear the question input.
         self.question = ""
-        # Yield here to clear the frontend input before continuing.
         yield
 
         for item in session:
-         if hasattr(item.choices[0].delta, "content"):
-            if item.choices[0].delta.content is None:
-                # presence of 'None' indicates the end of the response
-                break
-            answer += item.choices[0].delta.content
-            self.chat_history[-1] = (
-                self.chat_history[-1][0],
-                answer,
-                print(answer)
-                
-            )
+            if hasattr(item.choices[0].delta, "content"):
+                if item.choices[0].delta.content is None:
+                    break
+                answer += item.choices[0].delta.content
+                self.chat_history[-1] = (
+                    self.chat_history[-1][0],
+                    answer,
+                )
         yield
